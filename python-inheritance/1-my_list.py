341d9da0eafab8b37bedc6d2e1eda4e683b1f922
kethia-keza <k.keza@alustudent.com>
2026-08-05 10:24:23 -0700

    Add Task 1: MyList class and test cases

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    A class that inherits from list and adds a method to print
+    the list sorted.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order without modifying
+        the original list.
+        """
+        print(sorted(self))
