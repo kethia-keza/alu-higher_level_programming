9b74b6c50a13d863c756b07cbfe99adde71eadbe
kethia-keza <k.keza@alustudent.com>
2026-08-05 10:58:24 -0700

    Add Task 1: write_file function implementation

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains a function that writes a string to a text file (UTF8)
+and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns character count.
+
+    Args:
+        filename (str): The path/name of the file.
+        text (str): The string to write to the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
