1ba44a2ffb8a3ab0047a4fe0a239f6e3050267db
kethia-keza <k.keza@alustudent.com>
2026-08-07 00:32:31 -0700

    Add 16-no_link.sql

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Lists all records of second_table having a name value, ordered by score (top first)
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
