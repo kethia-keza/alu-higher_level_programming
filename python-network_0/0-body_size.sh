f55f5c92562d8c9143fe630529e38b3beb1b6c8e
kethia-keza <your-email@example.com>
2026-08-11 19:38:05 +0200

    Task 0: Use curl and wc -c to display body size in bytes

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request, and displays the size of the body of the response in bytes
-curl -s -o /dev/null -w '%{size_download}\n' "$1"
+# Displays the size of the body of the response of a given URL
+curl -s "$1" | wc -c
